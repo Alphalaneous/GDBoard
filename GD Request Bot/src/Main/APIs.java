415cf95e8f2267aa4da9c0c2683082ed6929b269
Ashton <38200084+Alphatism@users.noreply.github.com>
2020-06-12 12:07:47 -0400

    Error tests

--- GD Request Bot/src/Main/APIs.java
+++ GD Request Bot/src/Main/APIs.java
@@ -178,7 +178,7 @@ public class APIs {
 			String x = br.readLine();
 			return JsonObject.readFrom(x);
 		} catch (IOException e) {
-			e.printStackTrace();
+			JOptionPane.showMessageDialog(Overlay.frame, e, "Error", JOptionPane.ERROR_MESSAGE);
 			return null;
 		}
 	}
