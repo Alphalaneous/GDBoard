2c49a70585b75375578df941ee86ecdc63a77465
Ashton <38200084+Alphatism@users.noreply.github.com>
2020-12-11 17:26:39 -0500

    More GDHelper stuff

--- GD Request Bot/src/com/alphalaneous/GDHelper.java
+++ GD Request Bot/src/com/alphalaneous/GDHelper.java
@@ -6,9 +6,19 @@ public class GDHelper {
 
 	private static boolean isDead = true;
 	private static boolean isInLevel = false;
-	private static String levelName;
-	private static String creator;
-	private static long levelID;
+	private static String levelName = "";
+	private static String creator = "";
+	private static long levelID = 0;
+	private static int attemptCount = 0;
+	private static int normalBest = 0;
+	private static int practiceBest = 0;
+	private static int totalJumps = 0;
+	private static int totalAttempts = 0;
+	private static float percent = 0;
+	private static float posX = 0;
+	private static float posY = 0;
+	private static int levelLength = 0;
+	private static int objects = 0;
 
 	private static Runtime rt = Runtime.getRuntime();
 
@@ -67,7 +77,36 @@ public class GDHelper {
 							}
 							if (type.equalsIgnoreCase("ID")) {
 								levelID = Long.parseLong(response);
-
+							}
+							if (type.equalsIgnoreCase("Current Attempt")) {
+								attemptCount = Integer.parseInt(response);
+							}
+							if (type.equalsIgnoreCase("Total Jumps")) {
+								totalJumps = Integer.parseInt(response);
+							}
+							if (type.equalsIgnoreCase("Total Attempts")) {
+								totalAttempts = Integer.parseInt(response);
+							}
+							if (type.equalsIgnoreCase("Percent")) {
+								percent = Float.parseFloat(response);
+							}
+							if (type.equalsIgnoreCase("X")) {
+								posX = Float.parseFloat(response);
+							}
+							if (type.equalsIgnoreCase("Y")) {
+								posY = Float.parseFloat(response);
+							}
+							if (type.equalsIgnoreCase("Length")) {
+								levelLength = Integer.parseInt(response);
+							}
+							if (type.equalsIgnoreCase("Normal Best")) {
+								normalBest = Integer.parseInt(response);
+							}
+							if (type.equalsIgnoreCase("Practice Best")) {
+								practiceBest = Integer.parseInt(response);
+							}
+							if (type.equalsIgnoreCase("Object Count")) {
+								objects = Integer.parseInt(response);
 							}
 						}
 						//System.out.println(line);
@@ -116,5 +155,35 @@ public class GDHelper {
 	}
 	public static boolean getCurrentInLevelStatus(){
 		return isInLevel;
+	}
+	public static int getCurrentAttempts(){
+		return attemptCount;
+	}
+	public static int getTotalJumps(){
+		return totalJumps;
+	}
+	public static int getTotalAttempts(){
+		return totalAttempts;
+	}
+	public static float getPercent(){
+		return percent;
+	}
+	public static int getNormalBest(){
+		return normalBest;
+	}
+	public static int getPracticeBest(){
+		return practiceBest;
+	}
+	public static int getLength(){
+		return levelLength;
+	}
+	public static int getObjectCount(){
+		return objects;
+	}
+	public static float getX(){
+		return posX;
+	}
+	public static float getY(){
+		return posY;
 	}
 }
